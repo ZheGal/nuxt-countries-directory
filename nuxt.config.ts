043bef7a811501c7a994e1470fed8ac8bf9ba4df
Evgeniy Galkovskiy <zhegal@gmail.com>
2024-03-19 21:55:41 +0200

    Icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: 'apple-touch-icon', href: 'icons/apple-icon-180.png' }],
+      meta: [{ name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    },
+  },
   devtools: { enabled: true },
   ssr: true,
   css: ['~/assets/scss/main.scss'],
@@ -22,6 +28,32 @@ export default defineNuxtConfig({
       description: 'Nuxt3 PWA Countries List Application',
       theme_color: '#334155',
       background_color: '#FFFFFF',
+      icons: [
+        {
+          src: 'icons/manifest-icon-192.maskable.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'any',
+        },
+        {
+          src: 'icons/manifest-icon-192.maskable.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+        {
+          src: 'icons/manifest-icon-512.maskable.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any',
+        },
+        {
+          src: 'icons/manifest-icon-512.maskable.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
     },
     workbox: {
       navigateFallback: '/',
